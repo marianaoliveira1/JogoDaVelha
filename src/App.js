93ec9dafa07993ae5e7dacf0a2dfb7db390703be
marianaoliveirafernandes@alu.ufc.br <marianaoliveirafernandes@alu.ufc.br>
2022-04-20 16:48:10 -0300

    tela de home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Game } from "./components/Game";
+import styles from './styles/style.css';
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <h1>Jogo da Velha</h1>
+      <styles />
       <Game />
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
+function getInitialState() {
+    return {};
+}
+
 export function Game() {
+
+    const [values, setValues] = useState(getInitialState);
+
     return ( 
-        <div>Teste</div>
+        <div className="Game">
+            <div className="Game__board">
+                {
+                    Array.from({length: 9}).map((_, index) => (
+                        <button key={index} type="button">{index}</button>
+                    ))
+                }
+            </div>
+        </div>
+
     );
 }
